0b5548aa42d8bdc0a050ab8807e7be6cc389609e
Ademola-xx <ademola1145@gmail.com>
2022-02-15 23:37:04 +0100

    Integrate Actix web framework with existing todo app

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-mod state;
+use actix_web::{App, HttpServer};
+mod views;
 mod processes;
 mod to_do;
+mod state;
 
-use std::env;
-use state::read_file;
-use serde_json::value::Value;
-use serde_json::Map;
-use to_do::to_do_factory;
-use processes::process_input;
-
-fn main() {
-    // get variables from command line arguments 
-    let args: Vec<String> = env::args().collect();
-
-    //get command
-    let command: &String = &args[1];
-    //get title
-    let title: &String = &args[2];
-
-    //get state from json file
-    let state: Map<String, Value> = read_file("./state.json");
-
-    let status: String;
-
-    //get status if it exists in state, set status to pending if it doesn't exist
-    match &state.get(*&title) {
-        Some(result) => {
-            status = result.to_string().replace('\"', "");
-        }
-        None => {
-            status = String::from("pending");
-        }
-    }
-
-    // Creates item object for manipulation. it is an abstract representation of the todo components of our state.
-    let item = to_do_factory(&status, title).expect(&status);
-
-    
-    // If command is "Create" then it adds it to the state.
-    // If command is "Edit" then it replaces the field with which it shares a title in the state.
-    // If command is "Delete" then it deletes the field that shares its title.
-    // If command is "Get" then it gets the field that shares its title.
-    process_input(item, command.to_string(), &state);
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(
+            views::views_factory);
+        return app;
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
--- src/state.rs
+++ src/state.rs
@@ -17,11 +17,13 @@ pub fn read_file(file_name: &str) -> Map<String, Value> {
     let mut data = String::new();
     // read contents into String
     file.read_to_string(&mut data).unwrap();
-    // use serde's from_str() function. It returns a "Value" datatype
+    // use serde's from_str() function. It returns a "Value" datatype(represents json format) 
     let json: Value = serde_json::from_str(&data).unwrap();
     // convert the Value datatype into a Map<String, Value> and return
     let state: Map<String, Value> = json.as_object().unwrap().clone();
     return state;
+
+    // File -> String -> Json format -> Map<String, Value> (state)
 
 }
 
@@ -31,4 +33,6 @@ pub fn write_to_file(file_name: &str, state: &mut Map<String, Value>) {
     // converts back to a string and writes to file
     fs::write(file_name.to_string(), new_data.to_string()).expect(
         "Unable to write file");
+
+    // Map<String, Value> (state) -> Json format -> String -> File
 }
