117de35951ab95392a1934988cf256b68256d530
Ademola-xx <ademola1145@gmail.com>
2022-02-17 02:35:01 +0100

    fix minor error in main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,11 @@
 //this function takes what the user types into the input field and 
 //displays it as an alert
 
-
 function postAlert() {
-
-    function postAlert() {
-        // Gets reference to input field
-        let titleInput = document.getElementById("name");
-        // creates an alert that displays the input field's value
-        alert(titleInput.value);
-        // set input field value back to null
-        titleInput = null;
-    }
-
-    // get reference to create button
-    let createButton = document.getElementById("create-button");
-    // when button is clicked, call function postAlert
-    createButton.addEventListener("click", postAlert);
+    titleInput = document.getElementById("name");
+    alert(titleInput.value);
+    titleInput.value = null;
 }
+
+let createButton = document.getElementById("create-button");
+createButton.addEventListener("click", postAlert);
